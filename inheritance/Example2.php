5b9eac60de268582a856b8f4c585f082b67aaf83
akash <theakashsarkar@gmail.com>
2020-02-12 18:53:14 +0600

    Inheritance

--- inheritance/Example2.php
+++ inheritance/Example2.php
@@ -1,15 +1,16 @@
 <?php
-  class Animal{
+  class Animal 
+  {
       protected $legs;
       private $heand;
       private $ears;
       private $eyes;
 
       public function __constructor($legs,$heand,$ears,$eyes){
-          $this->legs = $legs;
+          $this->legs  = $legs;
           $this->heand = $heand;
-          $this->ears = $ears;
-          $this->eyes = $eyes;
+          $this->ears  = $ears;
+          $this->eyes  = $eyes;
           
         }  
         public function getLegs(){
@@ -24,15 +25,35 @@
         public function setHeand(){
             $this->heand = $heand;
         }
+        
+        public function showClass()
+        {
+          echo "funtion hellow world";
+        }
      
   }
 
   class Mamal extends Animal
   {
-    public function __constructor($legs,$heand,$ears,$eyes)
+    private $size;
+
+    public function __constructor($legs,$heand,$ears,$eyes,$size)
     {
         parent::__constructor($legs,$heand,$ears,$eyes);
+        $this->size = $size;
     }
+
+    public function showSize()
+    {
+      echo "size is $this->size";
+    }
+
+    public function showClass()
+    {
+      echo "function hellow world";
+    }
+
   }
-  $dog = new Mamal(4,0,2,2);
-  echo $dog->getHeand();
+
+  $dog = new Mamal(4,0,2,2,'big');
+  echo $dog->showClass();
