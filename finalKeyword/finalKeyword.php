e9484744e6270de742be9af85a3843ea2fdd3db7
akash_sarkar <theakashsarkar@gmail.com>
2020-10-17 11:43:20 +0600

    final keyword implement

---
+++ finalKeyword/finalKeyword.php
@@ -0,0 +1,15 @@
+<?php
+
+final class user {
+    public $name = "techMaster";
+    final public function userName()
+    {
+        echo "$this->name";
+    }
+}
+class userName {
+    public function overight()
+    {
+        echo "Hello world";
+    }
+}
